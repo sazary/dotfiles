51a383d4cc13d01f99d635810cf0fab1601b07db
sazary <soroosh@azary.ir>
2020-06-01 10:32:43 +0430

    Update fish plugins

--- fish/config.fish
+++ fish/config.fish
@@ -8,8 +8,6 @@ set -x PATH /Users/sazary/code/go/bin $PATH
 
 eval (python -m virtualfish)
 
-__fish_complete_django django-admin.py
-__fish_complete_django manage.py
 
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
